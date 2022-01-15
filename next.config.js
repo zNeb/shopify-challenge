fd6156b5074e016d0f92a0585f417de873062031
neb <habbouche.matthew@gmail.com>
2022-01-15 12:35:24 -0500

    Add Basic configs

--- next.config.js
+++ next.config.js
@@ -1,6 +1,23 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+// Analyse bundle
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
 
-module.exports = nextConfig
+// @ts-check
+
+/**
+ * @type {import('next').NextConfig}
+ * */
+const nextConfig = {
+  poweredByHeader: false,
+  reactStrictMode: true,
+  images: {
+    // Cache images
+    minimumCacheTTL: 2678400,
+    domains: [
+      'api.nasa.gov',
+    ],
+  },
+};
+
+module.exports = withBundleAnalyzer(nextConfig);

1010d3fd66a5881c1c9129c968adab91112dec8e
neb <habbouche.matthew@gmail.com>
2022-01-15 12:43:51 -0500

    Replace default page

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,3 +1,3 @@
 export default function Header() {
-    return <>Header</>
+  return <>Header</>;
 }
--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -1,12 +1,16 @@
-import type { ReactNode } from "react";
-import Footer from "../Footer";
-import Header from "../Header";
+import type { ReactNode } from 'react';
+import Footer from '../Footer';
+import Header from '../Header';
 
 interface Props {
-    children: ReactNode
+  children: ReactNode;
 }
-export default function Layout({children}) {
-    return (<>
-    <Header/>{children}<Footer/>
-    </>)
+export default function Layout({ children }: Props) {
+  return (
+    <>
+      <Header />
+      {children}
+      <Footer />
+    </>
+  );
 }

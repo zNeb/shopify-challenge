27813d8f6c419e4207e04fd00ef0705c6035b6d9
neb <habbouche.matthew@gmail.com>
2022-01-15 12:20:01 -0500

    Add header and footer

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -1,3 +1,12 @@
-export default function Layout() {
-    return <></>
+import type { ReactNode } from "react";
+import Footer from "../Footer";
+import Header from "../Header";
+
+interface Props {
+    children: ReactNode
+}
+export default function Layout({children}) {
+    return (<>
+    <Header/>{children}<Footer/>
+    </>)
 }

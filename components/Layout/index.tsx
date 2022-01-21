a90085bff38d118608ab0862bd340625d73a827b
neb <habbouche.matthew@gmail.com>
2022-01-21 11:33:45 -0500

    Add skip nav link

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -1,8 +1,10 @@
+import { SkipNavContent, SkipNavLink } from '@reach/skip-nav';
 import Progress from 'components/Progress';
 import Head from 'next/head';
 import type { ReactNode } from 'react';
 import Footer from '../Footer';
 import Header from '../Header';
+import '@reach/skip-nav/styles.css';
 
 interface Props {
   children: ReactNode;
@@ -19,7 +21,9 @@ export default function Layout({ children }: Props) {
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
       </Head>
+      <SkipNavLink />
       <Header />
+      <SkipNavContent />
       {children}
       <Footer />
       <Progress />
